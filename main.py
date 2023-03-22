6589edf6ac50dd8d0fbbebca82d88d96f309c021
Ryankwon03 <ryankwon@ucsb.edu>
2023-03-22 13:24:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("tesla.csv")
+movies = pd.read_csv("tmdb_5000_movies.csv")
 
 print(df.head())
-
-print("hello world")
+print(movies.head())
