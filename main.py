9ac9672242f239a733ee1b9043cdea7282f830a7
Ryankwon03 <ryankwon@ucsb.edu>
2023-03-22 13:28:50 -0700

    Testing files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("tesla.csv")
 movies = pd.read_csv("tmdb_5000_movies.csv")
+credits = pd.read_csv("tmdb_5000_credits.csv")
 
 print(df.head())
 print(movies.head())
+print(credits.head())
