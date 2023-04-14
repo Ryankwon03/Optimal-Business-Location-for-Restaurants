4667886108fa2dd7a57b0f76ad063eb6df8d551e
Ryankwon03 <ryankwon@ucsb.edu>
2023-04-13 22:19:07 -0700

    Create Crime_write.py

---
+++ Crime_write.py
@@ -0,0 +1,50 @@
+import requests
+import numpy as np
+import pandas as pd
+import csv
+
+def get_zipcode_from_lat_lng(latitude, longitude):
+    api_key = "AIzaSyDeY_8ameEGTbi-yW2z3VEkiVhjyqObVEU" # Replace with your API key
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={api_key}"
+    response = requests.get(url)
+    json_data = response.json()
+    for result in json_data['results']:
+        for component in result['address_components']:
+            if 'postal_code' in component['types']:
+                return component['short_name']
+    return None
+
+if __name__ == "__main__":
+    zip_codes = {}
+    data = pd.read_csv("Crime_Data_from_2020_to_Present.csv")
+    latitude = []
+    longitude = []
+    for i in data:
+        if(i == 'LAT'):
+            for j in data[i]:
+                latitude.append(j)
+        if(i == 'LON'):
+            for j in data[i]:
+                longitude.append(j)
+    
+    
+    for i in range(len(latitude)):
+        if (get_zipcode_from_lat_lng(latitude[i], longitude[i]) not in zip_codes):
+            zip_codes[get_zipcode_from_lat_lng(latitude[i], longitude[i])] = 1
+        else:
+            zip_codes[get_zipcode_from_lat_lng(latitude[i], longitude[i])] += 1
+        print(get_zipcode_from_lat_lng(latitude[i], longitude[i]), zip_codes[get_zipcode_from_lat_lng(latitude[i], longitude[i])])
+    
+# Open a new CSV file in write mode
+with open("cleaned_crime.csv", "w", newline="") as csvfile:
+    # Create a CSV writer object
+    writer = csv.writer(csvfile)
+    
+    # Write the header row
+    cnt = 0
+    for key, value in zip_codes.items():
+        print(cnt)
+        writer.writerow([key, value])
+        cnt = cnt + 1
+    
+
