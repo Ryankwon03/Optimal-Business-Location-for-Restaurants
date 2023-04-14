6c15c00757efa9af6a8a703d83721d6fe4204c36
mooonpatrick <97365542+mooonpatrick@users.noreply.github.com>
2023-04-13 21:50:06 -0700

    Create yoobintest.py

---
+++ yoobintest.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import csv
+
+
+data = pd.read_csv("la_population.csv")
+print(data)
+
+population = []
+for i in data.keys():
+    if(i == "Total Population, All Races"):
+        for j in data[i]:
+            population.append(j)
+
+
+for i in range(len(population)):
+    population[i] = int(population[i].replace(",", "")) 
+    print(population[i])
+
+
+
+for i in data.keys():
+    if(i != "Total Population, All Races") and (i[0] != "Z"):
+        for j in range(len(population)):
+            if('0' <= data[i][j][0] <= '9'):
+                data[i][j] = round(float(data[i][j][:-1]) * population[j] / 100.0)
+
+print(data)
+
+        
+            
+            #data[i][j] = float(data[i][j][:-1]) * population[j] / 100.0
+
+
+
+#print(data)
+#float(population) * float(percentage) / 100
