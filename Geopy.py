41ac3ad7e9595f262e1fb49b6f98704e78a4f718
Ryankwon03 <ryankwon@ucsb.edu>
2023-04-18 22:10:08 -0700

    Create Geopy.py

---
+++ Geopy.py
@@ -0,0 +1,38 @@
+import geopy
+from geopy.geocoders import Nominatim
+import numpy as np
+import pandas as pd
+import csv
+
+geolocator = Nominatim(user_agent="my_app")
+
+zip_codes = {}
+
+data = pd.read_csv("Crime_Data_from_2020_to_Present.csv")
+latitude = []
+longitude = []
+for i in data:
+    if(i == 'LAT'):
+        for j in data[i]:
+            latitude.append(j)
+    if(i == 'LON'):
+        for j in data[i]:
+            longitude.append(j)
+            
+for i in range(len(latitude)):
+    location = geolocator.reverse(f"{latitude[i]}, {longitude[i]}")
+    address = location.raw['address']
+    zipcode = address.get('postcode')
+    
+    if(zipcode not in zip_codes):
+        zip_codes[zipcode] = 1
+    else:
+        zip_codes[zipcode] += 1
+    print(zipcode, zip_codes[zipcode])
+
+    
+    
+
+
+
+print(zipcode)
